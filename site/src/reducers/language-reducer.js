edb691f7368f721dea8cf061a90691962a22bfdd
kitsoRik <gamedevrostik@gmail.com>
2020-07-03 14:50:23 +0300

    Closed

--- site/src/reducers/language-reducer.js
+++ site/src/reducers/language-reducer.js
@@ -1,14 +1,17 @@
 import { CHANGE_LANGUAGE_SUCCESS } from "../actions/language-actions";
 
-const initState = localStorage.getItem('language') || 'en';
+const initState = localStorage.getItem("language") || "en";
 
+document.documentElement.lang = initState;
 export default (state = initState, action) => {
-    switch(action.type) {
-        case CHANGE_LANGUAGE_SUCCESS: {
-            const { language } = action;
-            localStorage.setItem('language', language);
-            return language;
-        }
-        default: return state;
-    }
-}
+	switch (action.type) {
+		case CHANGE_LANGUAGE_SUCCESS: {
+			const { language } = action;
+			document.documentElement.lang = language;
+			localStorage.setItem("language", language);
+			return language;
+		}
+		default:
+			return state;
+	}
+};
